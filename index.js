5f54c40f98b43a1547d047bb4642ce35d90d9ace
Sofiko Chkadua <sofiachkadua@yandex-team.ru>
2020-09-22 00:23:27 +0300

    22.09

--- index.js
+++ index.js
@@ -1,26 +1,54 @@
-const modal__closedbutton = document.querySelector(".modal__closed-button");
-const intro__editbutton = document.querySelector(".intro__edit-button");
-const modal__savebutton = document.querySelector(".modal__save-button");
+// когда именуешь переменные, то делать это надо в camelCase, не важно как они называны в html
+// modal__closedbutton =>  modalClosedButton
+
+const modalClosedButton = document.querySelector(".modal__closed-button");
+// плюс название должно отображать смысл этой переменно
+// intro__editbutton => можно оставить editButton или openModalButton
+const editButton = document.querySelector(".intro__edit-button");
+// modalSubmitButton
+const modalSubmitButton = document.querySelector(".modal__save-button");
 const modal = document.querySelector(".modal");
-const intro__title = document.querySelector(".intro__title");
-const intro__subtitle = document.querySelector(".intro__subtitle");
-const input__title = document.querySelector(".modal__input");
-const input__subtitle = document.querySelector(".modal__last-input");
-const title = intro__title.textContent;
-const subtitle = intro__subtitle.textContent;
+// userTitle
+const userTitle = document.querySelector(".intro__title");
+// usersubtitle
+const userSubtitle = document.querySelector(".intro__subtitle");
+
+// userNameInput
+const userNameInput = document.querySelector(".modal__input_type_name");
+// userJobInput
+const userJobInput = document.querySelector(".modal__input_type_job");
+
+const form = document.querySelector(".modal__form");
+
+// вот это нужно перенести внутри функции и брать textContent каждый раз когда ты открываешь модалка
+// он всегда будет разный позле каждого нового сохранения
+// const title = intro__title.textContent;
+// const subtitle = intro__subtitle.textContent;
 
 
-modal__closedbutton.addEventListener("click", function() {
-  modal.classList.toggle("modal_closed");
-  input__title.value = title;
-  input__subtitle.value = subtitle;
+modalClosedButton.addEventListener("click", function() {
+  userNameInput.value = userTitle.textContent;
+  userJobInput.value = userSubtitle.textContent;
+  toggleModalWindow();
 });
 
-intro__editbutton.addEventListener("click", function(){
-  modal.classList.toggle("modal_closed");
-});
+editButton.addEventListener("click", toggleModalWindow);
 
-modal__savebutton.addEventListener("click", function(){
+form.addEventListener("submit", function(e){
+  e.preventDefault();
+ const name = userNameInput.value;
+ const job = userJobInput.value;
+ userTitle.textContent = name;
+ userSubtitle.textContent = job;
+  // если ты кликнешь на сохранить, то страница перезагрузится и сохранение профиля не произойдет
+  // перезагрузка - деволтное поведение когда нажимаешь на кнопку внутри формы, чтобы от него отказаться
+  // нужно написать e.preventDefault() , где e должен быть паарметром в фунции
+
+  // плюс ты не оюновляешь данные профиля, когда сохраняешь форму
     modal.classList.add("modal_closed");
   });
   
+function toggleModalWindow() {
+  modal.classList.toggle("modal_closed"); 
+}
+  // + вынести modal.classList.toggle("modal_closed"); в отдельную функцию toggleModalWindow и дергать ее каждый раз когда это нужно
